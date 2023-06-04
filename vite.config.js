794e16bae5c07843b509ef40fbcbda7ce5f9ffd3
Parhuam <44383266+parhuam-jalalian@users.noreply.github.com>
2023-06-04 14:04:49 -0700

    Testing stackoverflow suggestion

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'Personal-Portfolio/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "public/404.html"),
+      },
+    },
+  },
 })
