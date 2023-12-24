09c4c1ba0ad111d23c35aec4f9f340ad65c73a78
Joy Shaheb <khondokoralam@gmail.com>
2023-12-24 17:04:59 +0600

    experience field updated

--- components/Card/TimelineCard.tsx
+++ components/Card/TimelineCard.tsx
@@ -1,18 +1,33 @@
 import React from "react";
 
-const TimelineCard = () => {
+const TimelineCard = ({
+  company,
+  position,
+  from,
+  to,
+}: {
+  company: string;
+  position: string;
+  from: string;
+  to: string;
+}) => {
   return (
     <li className="mb-6 ms-4">
       <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-      <time className="mb-1 text-sm font-normal leading-none text-muted-foreground">
-        February 2022 - Present
-      </time>
-      <h3 className="text-lg font-semibold text-primary">
-        Application UI code in Tailwind CSS
+      <h3 className="text-xl font-semibold text-primary capitalize">
+        {company}
       </h3>
+      <h4 className="text-lg text-muted-foreground capitalize">{position}</h4>
+      <time className="mb-1 text-xs font-normal leading-none text-muted-foreground">
+        {from} - {to}
+      </time>
       <div className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
         <ol className="list-decimal ms-8 my-2">
-          <li>some work 1</li>
+          <li>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. At debitis
+            ullam, deleniti asperiores obcaecati eius quas. Dolorum,
+            repudiandae?
+          </li>
           <li>xyz responsibility</li>
           <li>played leadership role</li>
         </ol>
--- components/Sections/Experience.tsx
+++ components/Sections/Experience.tsx
@@ -8,11 +8,26 @@ const Experience = () => {
         <h1 className="title-bottom-line cursor-pointer text-2xl">
           Experience
         </h1>
-        <div className="">
+        <div className="flex justify-center">
           <ol className="relative border-s border-gray-200 dark:border-gray-700">
-            <TimelineCard />
-            <TimelineCard />
-            <TimelineCard />
+            <TimelineCard
+              company="abc company"
+              position="Senior software engineer"
+              from="2019"
+              to="present"
+            />
+            <TimelineCard
+              company="abc company"
+              position="Junior Frontend engineer"
+              from="2019"
+              to="present"
+            />
+            <TimelineCard
+              company="abc company"
+              position="software engineer"
+              from="2019"
+              to="present"
+            />
           </ol>
         </div>
       </div>
