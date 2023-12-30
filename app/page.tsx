9e9be75fc31864537455d2483027aab499c540a3
Joy Shaheb <khondokoralam@gmail.com>
2023-12-30 21:19:15 +0600

    removed console.log

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ const page = async () => {
   const data: IDataProps = await sanityClientFetch({
     query: dataQuery,
   });
-  console.log("xxxxx", data?.techSection);
+  // console.log("xxxxx", data?.techSection);
   return (
     <>
       <Navbar />
